819b821dcb982363c7a761c31b69a30103009d69
Gopi121202 <gopikagkumar02@gmail.com>
2025-07-24 20:54:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,7 +78,7 @@ def nav_bar():
     }
     </style>
     <div class="navbar">
-        <div style='position: absolute; left: 40px; color: black; font-weight: strongbold; font-size: 40px;'>Virtual EmoDash</div>
+        <div style='position: absolute; left: 30px; color: black; font-weight: strongbold; font-size: 40px;'>Virtual EmoDash</div>
         <a href="#" onclick="window.location.search='?nav=Emotion Capture'">Emotion Capture</a>
         <a href="#" onclick="window.location.search='?nav=Dashboard'">Dashboard</a>
         <a href="#" onclick="window.location.search='?nav=Data Log'">Data Log</a>
