7f21f2822dad104bd9fc0afb4be244c5c8832f09
Gopi121202 <gopikagkumar02@gmail.com>
2025-07-24 21:04:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,7 +78,7 @@ def nav_bar():
     }
     </style>
     <div class="navbar">
-        <div style='position: absolute; left: 35px; color: black; font-weight: strongbold; font-size: 45px;'>VIRTUAL EMODASH</div>
+        <div style='position: absolute; left: 35px; color: black; font-weight: bold; font-size: 35px;'>VIRTUAL EMODASH</div>
         <a href="#" onclick="window.location.search='?nav=Emotion Capture'">Emotion Capture</a>
         <a href="#" onclick="window.location.search='?nav=Dashboard'">Dashboard</a>
         <a href="#" onclick="window.location.search='?nav=Data Log'">Data Log</a>
@@ -97,10 +97,9 @@ def detect_emotion():
     st.subheader("📷 Capture Image")
     col1, col2 = st.columns([1, 2])
 
-    with col1:
-        st.markdown("""<div style='border: 2px solid #006d77; padding: 10px; border-radius: 10px; width: 100%; max-width: 350px; text-align: center;'>""", unsafe_allow_html=True)
+    with col1: 
         image = st.camera_input("Take a picture")
-        st.markdown("</div>", unsafe_allow_html=True)
+       
 
     with col2:
         if image:
