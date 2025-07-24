5efc81eab0e8350d85a180ed130d0a114b62ab81
Gopi121202 <gopikagkumar02@gmail.com>
2025-07-24 21:22:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,12 +37,11 @@ def set_plain_bg(image_path):
 
 def login_screen():
     set_plain_bg("background.png")
-   st.markdown("""
-    <h2 style='text-align: center; color: #006d77; font-weight: bold; margin-top: 30px;'>
-        🔐 STUDENT LOGIN
-    </h2>
-""", unsafe_allow_html=True)
-
+    st.markdown("""
+        <h2 style='text-align: center; color: #006d77; font-weight: bold; margin-top: 30px;'>
+            🔐 STUDENT LOGIN
+        </h2>
+    """, unsafe_allow_html=True)
     name = st.text_input("Enter your Name")
     sid = st.text_input("Enter your ID")
     login_btn = st.button("LOGIN")
